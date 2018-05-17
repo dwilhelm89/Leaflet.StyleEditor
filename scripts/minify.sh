9e4743927156cad07ceea91f3023070678e4ca34
fupduck <fupduck@users.noreply.github.com>
2018-05-17 18:25:57 +0200

    use travis to automatically minify js & css (#55)
    If a branch is merged to master and the css or the js are not minimized
    travis will automatically update min.js and min.css

---
+++ scripts/minify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# get script dir
+# https://stackoverflow.com/questions/192292/bash-how-best-to-include-other-scripts
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source "$DIR/travis-ci-git-commit.sh"
+
+# minimize and uglify css & js
+grunt
+
+# check if something changed
+if ! git diff-index --quiet HEAD --; then
+    # configure git
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+
+    # push changes
+    if travis-branch-commit; then
+        exit 0
+    else
+        exit 1
+    fi
+fi
+
+# terminate gracefully
+exit 0
