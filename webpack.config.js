fa1729924c0198c9df922863d38f46ef86ec8db1
fupduck <fupduck@users.noreply.github.com>
2020-01-07 21:59:01 +0100

    Use browserlists 'defaults' option (#93)
    * Use browserlists 'defaults' option
    
    Configure babel by settings targets to default.
    Currently this includes IE11 as well.

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,14 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  'targets': 'defaults'
+                }
+              ]
+            ]
           }
         }
       },
