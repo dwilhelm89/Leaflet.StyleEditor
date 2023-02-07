1f7286836ee4d79cbf1d79aa5926c98405a2c83e
Felix Höffken <felix@ctrl.alt.coop>
2023-02-07 16:28:36 +0100

    Correctly import Dom*

--- src/typescript/StyleForm.ts
+++ src/typescript/StyleForm.ts
@@ -1,6 +1,7 @@
 import { Form } from './forms'
 import { StyleEditorClass } from './StyleEditorClass'
 import { StyleEditor } from './StyleEditor'
+import { DomEvent, DomUtil } from 'leaflet'
 
 export class StyleForm extends StyleEditorClass {
 
@@ -17,8 +18,8 @@ export class StyleForm extends StyleEditorClass {
   }
 
   addDOMEvents() {
-    L.DomEvent.addListener(this.map as any, 'click', this.lostFocus, this)
-    L.DomEvent.addListener(this.styleEditor.editorUI, 'click', this.lostFocus, this)
+    DomEvent.addListener(this.map as any, 'click', this.lostFocus, this)
+    DomEvent.addListener(this.styleEditor.editorUI, 'click', this.lostFocus, this)
   }
 
   createForms() {
@@ -51,7 +52,7 @@ export class StyleForm extends StyleEditorClass {
       if (!parent) {
         break
       }
-      if (!!parent.className && L.DomUtil.hasClass(parent, 'leaflet-styleeditor-interior')) {
+      if (!!parent.className && DomUtil.hasClass(parent, 'leaflet-styleeditor-interior')) {
         return
       }
       parent = parent.parentNode
