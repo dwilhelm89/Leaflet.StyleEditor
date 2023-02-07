27eac0db79822f0390069b7f08a60f134c7c23b8
Felix Höffken <felix@ctrl.alt.coop>
2023-02-07 16:29:11 +0100

    Add more types

--- src/typescript/marker/MarkerOptions.ts
+++ src/typescript/marker/MarkerOptions.ts
@@ -1,6 +1,6 @@
 import { MarkerOptions as LMarkerOptions } from "leaflet"
 export interface MarkerOptions extends LMarkerOptions {
-  iconColor?
-  iconSize?
-  popupContent?
+  iconColor?: string // TODO color
+  iconSize?: string // TODO size type
+  popupContent? // TODO what is this used for?
 }
--- src/typescript/options/StyleEditorOptions.ts
+++ src/typescript/options/StyleEditorOptions.ts
@@ -4,18 +4,18 @@ import { MarkerForm, FormClass, PathForm, FillableForm } from '../forms'
 import { DefaultMarker, MarkerClass } from '../marker'
 
 export interface StyleEditorOptions extends ControlOptions {
-  colorRamp
-  defaultColor
+  colorRamp: string[] // TODO colors
+  defaultColor?: string // TODO color
 
   markerType: MarkerClass
-  markers
-  defaultMarkerIcon
-  defaultMarkerColor
+  markers?
+  defaultMarkerIcon?: string
+  defaultMarkerColor?: string //TODO color
 
   forms: FormClass[]
 
-  openOnLeafletDraw
-  openOnLeafletEditable
+  openOnLeafletDraw: boolean
+  openOnLeafletEditable: boolean
 
   styleEditorEventPrefix: string
 
@@ -32,12 +32,8 @@ export const DefaultStyleEditorOptions: StyleEditorOptions = {
   colorRamp: ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#16a085', '#27ae60', '#2980b9', '#8e44ad',
     '#2c3e50', '#f1c40f', '#e67e22', '#e74c3c', '#ecf0f1', '#95a5a6', '#f39c12', '#d35400', '#c0392b',
     '#bdc3c7', '#7f8c8d'],
-  defaultColor: null,
 
   markerType: DefaultMarker,
-  markers: null,
-  defaultMarkerIcon: null,
-  defaultMarkerColor: null,
 
   ignoreLayerTypes: [],
 
