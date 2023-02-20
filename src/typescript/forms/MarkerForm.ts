3b58b723e79ce8c89d9640ca87292b9739f87c91
Felix Höffken <felix@ctrl.alt.coop>
2023-02-20 14:23:20 +0100

    Lint MarkerForm

--- src/typescript/forms/MarkerForm.ts
+++ src/typescript/forms/MarkerForm.ts
@@ -10,14 +10,16 @@ import { StyleableLayer, Marker as LMarker } from 'leaflet';
 
 /** Form used to enable modification of a Geometry */
 export class MarkerForm extends Form {
-  whenToShow(layers: StyleableLayer[]): Boolean {
-    return layers.some((layer) => layer instanceof LMarker);
-  }
 
-  formElements: Record<string, FormElementClass> = {
+  protected formElements: Record<string, FormElementClass> = {
     icon: IconElement,
     color: ColorElement,
     size: SizeElement,
     popupContent: PopupContentElement,
   };
+
+  public whenToShow(layers: StyleableLayer[]): Boolean {
+    return layers.some((layer) => layer instanceof LMarker);
+  }
+
 }
