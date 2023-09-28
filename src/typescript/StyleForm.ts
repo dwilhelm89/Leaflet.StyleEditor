ef027d0e6c7f7a9b48fcb9a630900f24b2d9520b
Felix Höffken <felix@ctrl.alt.coop>
2023-09-28 23:04:48 +0200

    Remove function that does nothing

--- src/typescript/StyleForm.ts
+++ src/typescript/StyleForm.ts
@@ -1,7 +1,7 @@
 import { Form, FormClass } from './forms';
 import { StyleEditorClass } from './StyleEditorClass';
 import { StyleEditor } from './StyleEditor';
-import { DomEvent, DomUtil, Map } from 'leaflet';
+import { Map } from 'leaflet';
 
 export class StyleForm extends StyleEditorClass {
   styleEditorInterior: HTMLElement;
@@ -11,17 +11,6 @@ export class StyleForm extends StyleEditorClass {
   constructor(styleEditor: StyleEditor, map: Map, forms: FormClass[]) {
     super(styleEditor);
     this.createForms(forms);
-    this.addDOMEvents(map);
-  }
-
-  addDOMEvents(map: Map) {
-    DomEvent.addListener(map as unknown as HTMLElement, 'click', this.lostFocus, this);
-    DomEvent.addListener(
-      this.styleEditor.editorUI,
-      'click',
-      this.lostFocus,
-      this
-    );
   }
 
   createForms(forms: FormClass[]) {
@@ -44,19 +33,4 @@ export class StyleForm extends StyleEditorClass {
     this.forms.find((form: Form) => form.whenToShow(this.styleEditor.getCurrentLayers()))?.show()
   }
 
-  private lostFocus(e) {
-    let parent = e.target;
-    for (let i = 0; i < 10; i++) {
-      if (!parent) {
-        break;
-      }
-      if (
-        !!parent.className &&
-        DomUtil.hasClass(parent, 'leaflet-styleeditor-interior')
-      ) {
-        return;
-      }
-      parent = parent.parentNode;
-    }
-  }
 }
